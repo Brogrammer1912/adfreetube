19220919873ad7da0c67fdf6b5d52bcebd24a058
Akshath Sai, Pittala <akshathsai.pittala@outlook.com>
2025-05-06 18:25:22 +0530

    Add caching support for suggested videos in HomeController

--- src/main/java/akshathsaipittala/addfreetube/web/HomeController.java
+++ src/main/java/akshathsaipittala/addfreetube/web/HomeController.java
@@ -2,6 +2,7 @@ package akshathsaipittala.addfreetube.web;
 
 import akshathsaipittala.addfreetube.yt.YoutubeCrawler;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,7 @@ public class HomeController {
     final YoutubeCrawler youtubeCrawler;
 
     @GetMapping("/")
+    @Cacheable("suggested")
     public String home(Model model) {
         var videos = youtubeCrawler.getVideos("trailer");
         model.addAttribute("videos", videos);
