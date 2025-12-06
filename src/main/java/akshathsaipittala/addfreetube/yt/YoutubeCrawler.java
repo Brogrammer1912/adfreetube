eda7b43dc4c9f3f3395fb127c15857c8dc203a05
Akshath Sai, Pittala <akshathsai.pittala@outlook.com>
2025-12-06 18:47:52 +0530

    Refactor imports in YoutubeCrawler.java to use tools.jackson instead of com.fasterxml.jackson

--- src/main/java/akshathsaipittala/addfreetube/yt/YoutubeCrawler.java
+++ src/main/java/akshathsaipittala/addfreetube/yt/YoutubeCrawler.java
@@ -6,12 +6,12 @@ package akshathsaipittala.addfreetube.yt;
 
 import akshathsaipittala.addfreetube.resilience.RetryService;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.springframework.stereotype.Service;
+import tools.jackson.databind.JsonNode;
+import tools.jackson.databind.ObjectMapper;
 
 import java.util.ArrayList;
 import java.util.List;
